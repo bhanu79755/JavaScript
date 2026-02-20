05470a18fa5fdf5eef0f9f6c8f9263c1551c4dcb
bhanu755 <bhanushreeka2004@gmail.com>
2026-02-20 09:40:05 +0530

    string in java script

---
+++ first.js
@@ -0,0 +1,12 @@
+let name1="bhanu Shree"
+let name2="bhanu Shree"
+let name3="bhanu Shree"
+let name4="bhanu Shree"
+let name5="bhanu Shree"
+   
+// console.log("My Name is:", name1, "hello my name is",name2, "hello my name is",name3, "hello my name is",name4,"hello name is",name5)
+   
+  
+  // console.log("hello myname is",name1)
+  
+  console.log(`hello my name is ${name1}\n hello my name is ${name2}\n hello my name is ${name3}\n hello my name is ${name4}\n hello my name is ${name5}`)
---
+++ primitivedatatypes.js
@@ -0,0 +1,72 @@
+//Number int ,float,decimal
+
+// let a=100.4;
+// console.log(a)
+// console.log(typeof(a))
+
+//parseInt and parse float used to convert string to integer and float
+// let a="100.5646";
+//  let x=Number.parseFloat(a)
+//  console.log(x)
+
+// Enclosed in double quotes then data type is string
+// let a="100"
+// console.log(a)
+// console.log(typeof(a))
+
+
+//string to number and data type conversion
+// let a="123"
+// let x=Number(a)
+// console.log(x)
+// console.log(typeof(x))
+
+
+// interger to string and string data type
+// let y=100;
+// console.log(y)
+// console.log(typeof(y))
+// console.log(String(y))
+// console.log(typeof(String(y)))
+
+ 
+//NULL data type
+// let a=null;
+// a=100
+// console.log(a)
+// console.log(typeof(a))
+
+
+//  *undefined*
+// let a;
+// a="ravi"
+// console.log(a)
+// console.log(typeof(a))
+
+
+//*Boolean*
+// let a=Boolean(true)
+// let a=true
+// console.log(a)
+
+
+let a="ravi"
+console.log(a)
+let x=Number(a)
+console.log(x)
+console.log(typeof(x))
+console.log(typeof(typeof(x)))
+
+
+
+
+// type conversion and type casting
+// let a=Number(Boolean(NaN))
+// console.log(a)
+
+
+
+
+
+
+
