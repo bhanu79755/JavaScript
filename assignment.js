f9d37d5263c69c8e3f59726d9272b84c463787c1
bhanu79755 <bhanushreeka2004@gmail.com>
2026-02-25 08:50:31 +0530

    assignment operator

---
+++ assignment.js
@@ -0,0 +1,20 @@
+let a=10;
+let b=5;
+  // console.log(a=b) //5
+
+
+  // console.log(a+=b)///15 a=10+5
+  // console.log(a)
+
+  // console.log(a-=b)//5
+  // console.log(a+b)//10
+  // console.log(a) //5
+
+  // console.log(a*=b)//50
+  // console.log(a-b)//45
+
+
+  // console.log(a/=b)
+  // console.log(a)
+
+ console.log(a**=b)
